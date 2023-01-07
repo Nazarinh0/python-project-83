931ec58c35820ee1db345af9a46f21659a784bce
Nazar <nazar.annahalov@yandex.ru>
2023-01-08 00:52:22 +0300

    add content to db.py

--- page_analyzer/db.py
+++ page_analyzer/db.py
@@ -9,3 +9,34 @@ def connect():
     database = os.getenv('DATABASE_URL')
     conn = psycopg2.connect(database)
     return conn
+
+
+def exist_url(url):
+    with connect().cursor() as cursor:
+        cursor.execute(
+            """
+            SELECT id FROM urls
+            WHERE name = %(url)s;
+            """,
+            {'url': url})
+        id = cursor.fetchone()
+    if id:
+        return True
+    return False
+
+
+def find_url(id):
+    with connect().cursor() as cursor:
+        cursor.execute(
+            """
+            SELECT * FROM urls
+            WHERE id=%(id)s;
+            """,
+            {'id': id})
+        row = cursor.fetchone()
+    url_id, name, created_at = row
+    return {
+        'id': url_id,
+        'name': name,
+        'created_at': created_at
+    }
