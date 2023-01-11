2b75565d068be4e1f1debb64668f128ec3b12bca
Nazar <nazar.annahalov@yandex.ru>
2023-01-11 15:34:36 +0300

    fixes due to hexlet checks

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -65,6 +65,7 @@ def urls_check(id):
 
     try:
         response = requests.get(url)
+        response.raise_for_status()
     except requests.exceptions.RequestException as e:
         print(e)
         flash('Произошла ошибка при проверке', 'danger')
